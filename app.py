b0b4cb6d32bfb1ff72c175b02c6adbe3066d5e8b
ts170 <tim.stefany@gmx.de>
2022-05-27 17:07:34 +0200

    updating layout with bootstrap components

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+import struct
+
 from dash import Dash, html, dcc, Input, Output
+import dash_bootstrap_components as dbc
 import plotly.express as px
 import pandas as pd
 from wordcloud import WordCloud
@@ -8,7 +11,8 @@ import ast
 # ignore setting on copy warning
 pd.options.mode.chained_assignment = None  # default='warn'
 
-app = Dash(__name__)
+app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+template = "simple_white"
 
 # read csv files to dataframes
 df_prep = pd.read_csv('data/results_df_prep.csv')
@@ -18,48 +22,66 @@ df_comb = pd.read_csv('data/results_top_combinations.csv')
 countries = df_prep['Country'].unique()
 
 # create HTML layout including all input and output components
-app.layout = html.Div([
+app.layout = dbc.Container([
     html.Div([
-        html.H1(children='Dashboard Data Science - Travel Location Finder'),
-        # country filter for entire dashboard
-        html.Div([
-            html.Label('Countries'),
-            dcc.Dropdown(
-                countries,
-                id='global_countries'
-            )
-        ])
-    ]),
-    # all four graphs in grid order
-    html.Div([
-        # section for first two graphs
-        html.Div([
-            dcc.Graph(
-                id='wordcloud_ne',
-            ),
-            dcc.Graph(
-                id='wordcloud_no_ne'
-            )
-        ], style={'display': 'flex', 'flex-direction': 'row'}),
-        # section for second two graphs
-        html.Div([
-            dcc.Graph(
-                id='attraction_combinations',
-            ),
-            dcc.Graph(
-                id='individual_visualisation'
-            )
-        ], style={'display': 'flex', 'flex-direction': 'row'})
+        dbc.Row(dbc.Col(
+            html.Div([
+                html.H1(children='Dashboard Data Science - Travel Location Finder'),
+                # country filter for entire dashboard
+                html.Div([
+                    html.Label('Countries'),
+                    dcc.Dropdown(
+                        countries,
+                        id='global_countries',
+                        style={'with': '50%'}
+                    )
+                ])
+            ])
+        )),
+        dbc.Row(
+            [
+                dbc.Col(
+                    html.Div([
+                        dcc.Graph(
+                            id='wordcloud_ne',
+                        )
+                    ])
+                ),
+                dbc.Col(
+                    html.Div([
+                        dcc.Graph(
+                            id='wordcloud_no_ne',
+                        )
+                    ])
+                )
+            ]
+        ),
+        dbc.Row(
+            [
+                dbc.Col(
+                    dcc.Graph(
+                        id='attraction_combinations',
+                    )
+                ),
+                dbc.Col(
+                    dcc.Graph(
+                        id='individual_visualisation'
+                    )
+                )
+            ]
+        ),
     ])
 ])
+
+
 # callback for global continent filtering
 @app.callback(
     Output(component_id='wordcloud_ne', component_property='figure'),
     Output(component_id='wordcloud_no_ne', component_property='figure'),
     Output(component_id='attraction_combinations', component_property='figure'),
-    #Output(component_id='individual_visualisation', component_property='figure'),
+    # Output(component_id='individual_visualisation', component_property='figure'),
     Input(component_id='global_countries', component_property='value')
-    )
+)
 def apply_global_filter(global_country):
     # filter Data
     if global_country is not None:
@@ -85,13 +107,22 @@ def apply_global_filter(global_country):
     ne_img = WordCloud().fit_words(ne_dict)
     no_ne_img = WordCloud().fit_words(no_ne_dict)
 
-    wordcloud_ne = px.imshow(ne_img)
-    wordcloud_no_ne = px.imshow(no_ne_img)
+    wordcloud_ne = px.imshow(ne_img, title='Named Entities', template=template) \
+        .update_xaxes(visible=False).update_yaxes(visible=False)
+    wordcloud_ne.update_layout(margin={"l": 0, "r": 0, "t": 40, "b": 0})
+    wordcloud_no_ne = px.imshow(no_ne_img, title='No Named Entities', template=template) \
+        .update_xaxes(visible=False).update_yaxes(visible=False)
+    wordcloud_no_ne.update_layout(margin={"l": 0, "r": 0, "t": 40, "b": 0})
 
     # barchart for pairings
-    # sort dataframe for support
+    # sort dataframe for support and remove ' in tuple
     df_filter_pairs.sort_values(by='support', ascending=False, inplace=True)
-    comb = px.bar(df_filter_pairs.head(10), y='support', x='itemsets', title='popular pairs')
+    df_filter_pairs['itemsets'] = df_filter_pairs['itemsets'].str.replace("'", '')
+    # create horizontal barchart with acending order
+    comb = px.bar(df_filter_pairs.head(10), y='itemsets', x='support', title='popular pairs', template=template,
+                  orientation='h')
+    comb.update_layout(yaxis={'categoryorder': 'total ascending'})
+
     return wordcloud_ne, wordcloud_no_ne, comb
 
     # barchart for attraction category count
