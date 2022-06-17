9b54ce395dba87a20e3eaec52772f7510cffe612
SamuelPassauer <Samuel.passauer@yahoo.com>
2022-06-17 14:27:44 +0200

    update wordcloud

--- Task 2/task2d_dashboard.py
+++ Task 2/task2d_dashboard.py
@@ -4,7 +4,10 @@ import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 import plotly.express as px
+import stylecloud as sc
+from matplotlib import pyplot as plt
 from wordcloud import WordCloud
+import PIL.Image
 import pandas as pd
 import numpy as np
 
@@ -19,6 +22,9 @@ from datetime import datetime as dt
 TEAM_MEMBERS = (
     "Jennifer", "Henry", "Samuel", "Tim"
 )
+
+df = px.data.tips()
+
 
 """         Dashboard           """
 # Initialize app
@@ -71,7 +77,7 @@ app.layout = dbc.Container([
                         children=[
                         html.Div(id="clustering_graph_div",
                                  children=[
-                                     dcc.Graph(id="clustering_graph", figure=px.scatter())
+                                     dcc.Graph(id="clustering_graph", figure=px.scatter(template="plotly_dark"))
                                  ]
                         )
                         ]
@@ -117,7 +123,7 @@ app.layout = dbc.Container([
                          children=[
                              html.Div(id="topic_graph_div",
                                       children=[
-                                          dcc.Graph(id="topic_graph", figure={})
+                                          dcc.Graph(id="topic_graph")
                                       ]
                                       )
                          ]
@@ -148,19 +154,36 @@ app.layout = dbc.Container([
                                          html.Div(id="ddf_input_div",
                                                   children=[
                                                       dcc.Input(
-                                                          id="input_{}".format(_),
+                                                          id="input_Jennifer",
                                                           type="text",
-                                                          placeholder="Preference of {}".format(_),
+                                                          placeholder="Preference Jennifer",
+                                                          value="Preference Jennifer"
+                                                      ),
+                                                      dcc.Input(
+                                                          id="input_Henry",
+                                                          type="text",
+                                                          placeholder="Preference Henry",
+                                                          value="Preference Henry"
+                                                      ),
+                                                      dcc.Input(
+                                                          id="input_Samuel",
+                                                          type="text",
+                                                          placeholder="Preference Samuel",
+                                                          value="Preference Samuel"
+                                                      ),
+                                                      dcc.Input(
+                                                          id="input_Tim",
+                                                          type="text",
+                                                          placeholder="Preference Tim",
+                                                          value="Preference Tim"
                                                       )
-                                                      for _ in TEAM_MEMBERS
                                                   ]
 
                                                   )
                                      ]
                                  ),
                                  html.Div(
-                                     "Dream Destination Finder",
-                                     id="ddf")
+                                     id="ddf_container", children=[])
                              ]
                          ),
                          html.Div(
@@ -168,7 +191,7 @@ app.layout = dbc.Container([
                              children=[
                                  html.Div(id="ddf_graph_div",
                                           children=[
-                                              dcc.Graph(id="ddf_graph", figure={})
+                                              dcc.Graph(id="ddf_graph", figure=px.bar(df, x="day", y="total_bill", color="sex", template="plotly_dark"))
                                           ]
                                           )
                              ]
@@ -213,7 +236,9 @@ app.layout = dbc.Container([
                              children=[
                                  html.Div(id="ddacm_graph_div",
                                           children=[
-                                              dcc.Graph(id="ddacm_graph", figure=px.bar())
+                                              dcc.Graph(
+                                                  id="ddacm_graph",
+                                                  figure=px.bar(df, x="day", y="total_bill", color="sex", template="plotly_dark"))
                                           ]
                                           )
                              ]
@@ -224,33 +249,76 @@ app.layout = dbc.Container([
         ]
     ),
     dbc.Row(
-        "Autoren: Jennifer Hammen, Henry Mangold, Samuel Passauer, Tim Stefany",
+        "Authors: Jennifer Hammen, Henry Mangold, Samuel Passauer, Tim Stefany  |    Students at Hochschule der Medien  |    Data from: https://www.roughguides.com",
         style={"text-align":"center","height":"2%"}
     )
 
 ])
 
-# Callbacks
-'''@app.callback(
-    [Output(component_id="clustering_container"), Output(component_id="clustering_graph"),
-     Input(component_id="clustering_dropdown")]
-)
-
+"""         Dashboard           """
+# Callback Clustering Model
 @app.callback(
-    [Output(component_id="topic_container"), Output(component_id="topic_graph"),
-     Input(component_id="topic_dropdown")]
+    Output(component_id= "clustering_container", component_property="children"),
+    Input(component_id="dropdown_clustering", component_property="value")
+
 )
+def update_graph(input):
+    output=input
+    return output
 
+# Callback Topic Model
 @app.callback(
-    [Output(component_id="ddf_container"), Output(component_id="ddf_graph"),
-     Input(component_id="ddf_dropdown")]
-)'''
+    Output(component_id= "topic_graph", component_property="figure"),
+    [Input(component_id="input_Jennifer", component_property="value"),
+     Input(component_id="input_Henry", component_property="value"),
+     Input(component_id="input_Samuel", component_property="value"),
+     Input(component_id="input_Tim", component_property="value")]
+)
+def update_graph(preference_Jennifer, preference_Henry, preference_Samuel, preference_Tim):
+    input_wordcloud = str(preference_Jennifer) + str(preference_Henry) + str(preference_Samuel) + str(preference_Tim)
+    print(input_wordcloud)
+    wordcloud = WordCloud().generate(input_wordcloud)
+    plt.figure(figsize=(2,1))
+    plt.imshow(wordcloud, interpolation="bilinear")
+    plt.axis("off")
+    wordcloud_img = px.imshow(wordcloud).update_xaxes(visible=False).update_yaxes(visible=False)
+    wordcloud_img.update_layout(margin={"l": 0, "r": 0, "t": 0, "b": 0}, hovermode=False)
+    return wordcloud_img
+
+#Code to create wordclouds
+"""sample_text = "Though its cities draw the most tourists – New York, New Orleans, Miami, Los Angeles and San Francisco are all incredible destinations in their own right – America is above all a land of stunningly diverse and achingly beautiful landscapes . In one nation you have the mighty Rockies and spectacular Cascades, the vast, mythic desert landscapes of the Southwest, the endless, rolling plains of Texas and Kansas, the tropical beaches and Everglades of Florida, the giant redwoods of California and the sleepy, pristine villages of New England. You can soak up the mesmerizing vistas in Crater Lake, Yellowstone and Yosemite national parks, stand in awe at the Grand Canyon, hike the Black Hills, cruise the Great Lakes, paddle in the Mississippi, surf the gnarly breaks of Oahu and get lost in the vast wilderness of Alaska. Or you could easily plan a trip that focuses on the out-of-the-way hamlets, remote prairies, eerie ghost towns and forgotten byways that are every bit as “American” as its showpiece icons and monuments. The sheer size of the country prevents any sort of overarching statement about the typical American experience, just as the diversity of its people undercuts any notion of the typical American. Icons as diverse as Mohammed Ali, Louis Armstrong, Sitting Bull, Hillary Clinton, Michael Jordan, Madonna, Martin Luther King, Abraham Lincoln, Elvis Presley, Mark Twain, John Wayne and Walt Disney continue to inspire and entertain the world, and everyone has heard of the blues, country and western, jazz, rock ’n’ roll and hip-hop – all American musical innovations. There are Irish Americans, Italian Americans, African Americans, Chinese Americans and Latinos, Texan cowboys and Bronx hustlers, Seattle hipsters and Alabama pastors, New England fishermen, Las Vegas showgirls and Hawaiian surfers. Though it often sounds clichéd to foreigners, the only thing that holds this bizarre federation together is the oft-maligned “American Dream”. While the USA is one of the world’s oldest still-functioning democracies and the roots of its European presence go back to the 1500s, the palpable sense of newness here creates an odd sort of optimism, wherein anything seems possible and fortune can strike at any moment.Indeed, aspects of American culture can be difficult for many visitors to understand, despite the apparent familiarity: its obsession with guns; the widely held belief that “government” is bad; the real, genuine pride in the American Revolution and the US Constitution, two hundred years on; the equally genuine belief that the USA is the “greatest country on earth”; the wild grandstanding of its politicians (especially at election time); and the bewildering contradiction of its great liberal and open-minded traditions with laissez-faire capitalism and extreme cultural and religious conservatism. That’s America: diverse, challenging, beguiling, maddening at times, but always entertaining and always changing. And while there is no such thing as a typical American person or landscape, there can be few places where strangers can feel so confident of a warm reception. "
+mask = np.array(PIL.Image.open("Weltkarte.jpeg"))
+wordcloud = WordCloud(mask=mask).generate(sample_text)
+plt.figure(figsize=(10,5))
+plt.imshow(wordcloud, interpolation="bilinear")
+plt.axis("off")
+plt.savefig("wordcloud.png")
+fig = PIL.Image.open("wordcloud.png")
+fig.show()"""
+
+# Callback Dream  Destination Finder
+@app.callback(
+    Output(component_id= "ddf_container", component_property="children"),
+    [Input(component_id="input_Jennifer", component_property="value"),
+     Input(component_id="input_Henry", component_property="value"),
+     Input(component_id="input_Samuel", component_property="value"),
+     Input(component_id="input_Tim", component_property="value")]
+
+)
+def update_graph(preference_Jennifer, preference_Henry, preference_Samuel, preference_Tim):
+    preferences = []
+    preferences.append(preference_Jennifer)
+    preferences.append(preference_Henry)
+    preferences.append(preference_Samuel)
+    preferences.append(preference_Tim)
+    print(type(preferences))
+    print(preferences)
+    return preferences
 
 
 
-def update_graph():
-    graph= 'graph'
-    print(graph)
+
+
 
 if __name__ == "__main__":
     app.run_server(debug=True)
